b42488f93c77de530a1f4e0c6610cb4e62ff1960
Nathan Conlon <nathanec@gmail.com>
2021-08-30 20:02:33 -0500

    yaml stuff.

--- lib/dice.dart
+++ lib/dice.dart
@@ -51,43 +51,5 @@ Future<void> diceCommand(CommandContext ctx, String content) async {
     total_dice.clear();
     wrath_total.clear();
   }
-
-  /*await ctx.sendMessage(
-      content: 'Rolled ${rolled.finalResult}, ${rolled.rollNotation}');
-  await ctx.sendMessage(
-      content:
-          '${await dieFace(rolled.results[0].results[0])} ${await dieFace(rolled.results[0].results[1])} ${await dieFace(rolled.results[0].results[2])} ');*/
 }
 
-Future<String> dieFace(int face) async {
-  var newFace;
-  switch (face) {
-    case 1:
-      newFace = IGuildEmoji.fromId('838208311135830057')
-          .id; // https://cdn.discordapp.com/emojis/838208311135830057.png?v=1
-      break;
-    case 2:
-      newFace = IGuildEmoji.fromId('838208311114858526')
-          .id; // https://cdn.discordapp.com/emojis/838208311114858526.png?v=1
-      break;
-    case 3:
-      newFace = IGuildEmoji.fromId('838208311144873984')
-          .id; // https://cdn.discordapp.com/emojis/838208311144873984.png?v=1
-      break;
-    case 4:
-      newFace = IGuildEmoji.fromId('838220704699908118')
-          .id; // https://cdn.discordapp.com/emojis/838220704699908118.png?v=1
-      break;
-    case 5:
-      newFace = IGuildEmoji.fromId('838208311135830058')
-          .id; // https://cdn.discordapp.com/emojis/838208311135830058.png?v=1
-      break;
-    case 6:
-      newFace = IGuildEmoji.fromId('838208310930702349')
-          .id; // https://cdn.discordapp.com/emojis/838208310930702349.png?v=1
-      break;
-  }
-
-  print(newFace);
-  return '<:dice$face:$newFace>';
-}
