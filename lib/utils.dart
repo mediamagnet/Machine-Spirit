11a58e33e9ff37e3df74bb9a1d5eb4f7c9f0bdb7
Nathan Conlon <nathanec@gmail.com>
2021-09-03 01:30:53 -0500

    removed util function

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,24 +1,9 @@
 import 'dart:io' show Platform;
 // import 'dart:convert';
-import 'package:toml/loader/fs.dart';
+
 
 String get dartVersion {
   final platformVersion = Platform.version;
   return platformVersion.split('(').first;
 }
 
-Future<String> helpCommandGen(String commandName, String description,
-    {required String additionalInfo}) async {
-  var cfg = await loadConfig('config.toml');
-  final buffer = StringBuffer();
-
-  buffer.write("${cfg['Bot']['Prefix']}commandName**");
-
-  if (additionalInfo != null) {
-    buffer.write(' `$additionalInfo`');
-  }
-
-  buffer.write(' - $description.\n');
-
-  return buffer.toString();
-}
